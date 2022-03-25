09906681c1e4cc04b646953ce8bc9c1ecc4e5a37
kortessis <69532652+kortessis@users.noreply.github.com>
2022-03-25 11:23:11 -0400

    Add files via upload

--- Ecosystem_Eqm_Sim.m
+++ Ecosystem_Eqm_Sim.m
@@ -1,8 +1,6 @@
 clear
 clc
 
-% This script plots 
-
 %% Consider Strong and Weak Tradeoffs
 
 % Strong Tradeoffs
@@ -30,11 +28,16 @@ pP = 1 - pS/T_P;
 yP = R0P*(1 - sP*(1-gP))/(gP*eP*(f + pS/(1-pP)));
 
 % Litter production parameters
-bA = 0.5; bPprime = 0.1; bP = 0.1; 
-delta = (T_P*bPprime/bP - 1)/(T_P+1);
+bA = 0.5; bPprime = 0.1; 
+% Here we choos bPprime, which is a composite parameter that acts just like
+% bA. Once we have chosen bPprime, we choose values of bP and delta that
+% give bPprime (once already choosing T_P above).
+bP = 0.1; delta = bPprime/bP - 1/T_P;
 
 % Competition parameters
 alphaA = 0.1;   alphaPprime = 0.1;
+% Here we fix alphaPprime, which is a composite parameter that acts like
+% alphaA, and then choose values of alphaP and gamma that give alphaPprime. 
 alphaP = 0.1; gamma = (alphaPprime/alphaP - 1)*T_P;
 
 % Litter sensitivity parameters
@@ -47,8 +50,9 @@ bT = 0;
 
 gen = 100000;
 
-
 eqm = zeros(4,length(d));
+
+LeqmCoex = (R0A - R0P)/(betaA*R0P - betaP*R0A);
 
 s = [sA, sP, pS, pP];   y = [yA, yP, f];    g = [gA, gP];
 e = [eA, eP];           alpha = [alphaA, alphaP, gamma];
@@ -69,8 +73,8 @@ for i = 1:length(d)
     eqm(:,i) = sys(:,end);
 end
 
-dcrit(1) = d(find(log(eqm(1,:)) > -2,1));
-dcrit(2) = d(find(log(ones(1,2)*eqm(3:4,:)) < -2, 1));
+dcritP = 1/LeqmCoex*(bT + bA/alphaA*(R0A/(1 + betaA*LeqmCoex) - 1));
+dcritA = 1/LeqmCoex*(bT + bP*(delta + 1/T_P)/(alphaP*(1+gamma/T_P))*(R0P/(1+betaP*LeqmCoex) - 1));
 
 %% Figures 
 
@@ -79,20 +83,24 @@ subplot(1,2,2)
 plot(d, eqm(2,:), 'Color', 'black', 'LineWidth', 3);
 xlabel('Decomposition Fraction, {\itd}')
 ylabel('Equilbirium Litter Density');
-xline(dcrit, '--');
+xline(dcritA, '--');
+xline(dcritP, '--');
 ax = gca; ax.FontSize = 25; ax.FontName = 'Times New Roman';
 
-for i = 1:length(d)
-    Leqm_NoA(i) = LitterEq(R0P, alphaPprime, betaP, bPprime/d(i), bT/d(i));
-    Leqm_NoP(i) = LitterEq(R0A, alphaA, betaA, bA/d(i), bT/d(i));
-end
-
-hold on
-plot(d, Leqm_NoA)
-plot(d, Leqm_NoP)
-LeqmCoex = -(R0A - R0P)/(betaP*R0A - betaA*R0P);
-yline(LeqmCoex)
-hold off
+ % Uncomment these lines if you want to see the resident equlibrium litter
+ % for each species. They track the simulation results exactly except where
+ % the two species coexist.
+ 
+% for i = 1:length(d)
+%     Leqm_NoA(i) = LitterEq(R0P, alphaPprime, betaP, bPprime/d(i), bT/d(i));
+%     Leqm_NoP(i) = LitterEq(R0A, alphaA, betaA, bA/d(i), bT/d(i));
+% end
+% 
+% hold on
+% plot(d, Leqm_NoA)
+% plot(d, Leqm_NoP)
+% yline(LeqmCoex)
+% hold off
 
 
 %% Weak Tradeoffs
@@ -100,7 +108,7 @@ bPprime = 0.1;
 
 % Litter production parameters
 bA = 0.1; bP = 0.1; 
-delta = (T_P*bPprime/bP - 1)/(T_P+1);
+delta = bPprime/bP - 1/T_P;
 
 % Competition parameters
 alphaA = 0.1;   alphaPprime = 0.1;
@@ -129,9 +137,8 @@ end
 
 %% Figures 
 
-
-dcrit(1) = d(find(log(eqm(1,:)) > -2,1));
-dcrit(2) = d(find(log(ones(1,2)*eqm(3:4,:)) < -2, 1));
+dcritP = 1/LeqmCoex*(bT + bA/alphaA*(R0A/(1 + betaA*LeqmCoex) - 1));
+dcritA = 1/LeqmCoex*(bT + bP*(delta + 1/T_P)/(alphaP*(1+gamma/T_P))*(R0P/(1+betaP*LeqmCoex) - 1));
 
 
 subplot(1,2,1)
@@ -139,16 +146,19 @@ plot(d, eqm(2,:), 'Color', 'black', 'LineWidth', 3);
 xlabel('Decomposition Fraction, {\itd}')
 ylabel('Equilbirium Litter Density');
 ax = gca; ax.FontSize = 25; ax.FontName = 'Times New Roman';
-xline(dcrit, '--')
+xline(dcritA, '--')
+xline(dcritP, '--')
 
-for i = 1:length(d)
-    Leqm_NoA(i) = LitterEq(R0P, alphaPprime, betaP, bPprime/d(i), bT/d(i));
-    Leqm_NoP(i) = LitterEq(R0A, alphaA, betaA, bA/d(i), bT/d(i));
-end
+ % Uncomment these lines if you want to see the resident equlibrium litter
+ % for each species. They track the simulation results exactly.
+ 
+% for i = 1:length(d)
+%     Leqm_NoA(i) = LitterEq(R0P, alphaPprime, betaP, bPprime/d(i), bT/d(i));
+%     Leqm_NoP(i) = LitterEq(R0A, alphaA, betaA, bA/d(i), bT/d(i));
+% end
 
-hold on
-plot(d, Leqm_NoA)
-plot(d, Leqm_NoP)
-LeqmCoex = -(R0A - R0P)/(betaP*R0A - betaA*R0P);
-yline(LeqmCoex)
-hold off
+% hold on
+% plot(d, Leqm_NoA)
+% plot(d, Leqm_NoP)
+% yline(LeqmCoex)
+% hold off
